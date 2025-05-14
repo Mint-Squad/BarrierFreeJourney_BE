9edd98a98a4b684bb8bfb592729c6001a50cf00a
YuYeon Jeong <wjddbdus3696@naver.com>
2025-05-15 00:33:48 +0900

    change DB from sqlite3 to postgresql

--- APAC_BarrierFreeJourney/settings.py
+++ APAC_BarrierFreeJourney/settings.py
@@ -112,8 +112,8 @@ LOGGING = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': BASE_DIR / 'db.postgresql',
     }
 }
 
