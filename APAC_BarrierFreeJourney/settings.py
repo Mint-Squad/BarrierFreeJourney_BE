c7e15de4e4826054f8cd58b867263a099201592f
YuYeon Jeong <wjddbdus3696@naver.com>
2025-05-14 17:00:24 +0900

    add gunicorn and set secret key

--- APAC_BarrierFreeJourney/settings.py
+++ APAC_BarrierFreeJourney/settings.py
@@ -41,8 +41,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-19e#_-=#1wlnf_+wv@fks$yv6gxiav&=8n73-hg%6(tbrw2p*u'
-
+SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
